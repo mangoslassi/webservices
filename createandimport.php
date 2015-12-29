2fa5095b6b4ee7faa95807be2068f00e86dbbad9
Mangos Lassi <mangoslassi@gmail.com>
2015-12-29 06:05:03 -0500

    Several improvements and reduction of complexity.

--- createandimport.php
+++ createandimport.php
@@ -5,6 +5,9 @@ $soapPassword = 'testpass';
 $soapHost = '127.0.0.1';
 $soapPort = '7878';
 
+$dbUsername = 'mangos';
+$dbPassword = 'password';
+
 $client = new SoapClient(NULL, array(
 	'location'	=>	"http://$soapHost:$soapPort/",
 	'uri'		=>	'urn:MaNGOS',
@@ -16,7 +19,7 @@ $client = new SoapClient(NULL, array(
 $account_name = $_POST['account_name'];
 $account_pw = $_POST['account_pw'];
 
-$character_name = $_POST['character_name'];
+$character_name = ucfirst($_POST['character_name']);
 $character_contents = $_POST['luafile_contents'];
 
 $character_race = $_POST['character_race'];
@@ -77,33 +80,65 @@ if(!isset($result)) {
 	return 1;
 }
 
-$command = 'character level ' . $character_name . ' 60';
+unset($result);
 
-try {
-	$result = $client->executeCommand(new SoapParam($command, 'command'));
-} catch(Exception $e) {
-	echo 'Character level exception: ', $e->getMessage(), "\n";
-	return 1;
+function levelCharacter($attempts) {
+	global $client;
+	global $character_name;
+
+	echo "Calling levelCharacter...<br>\n";
+	$command = 'character level ' . $character_name . ' 60';
+	echo $command . "<br>";
+	try {
+		$result = $client->executeCommand(new SoapParam($command, 'command'));
+	} catch(Exception $e) {
+		echo 'Character level exception: ', $e->getMessage(), "\n<br>";
+		if($attempts > 0) {
+			levelCharacter($attempts - 1);
+		} else {
+			return 1;
+		}
+	}
+	if(!isset($result)) {
+		echo "Character level set failed!<br>\n";
+		return 1;
+	}
 }
 
-if(!isset($result)) {
-	echo "Character level set failed!<br>\n";
-	return 1;
-}
+// Write does not take place immediately so try to level the character repeatedly until it succeeds.
+
+levelCharacter(200);
 
 foreach($item_arr as $item) {
 	$command = 'send items ' . $character_name . ' "Imported Item" "Enjoy!" ' . $item . ':count1';
+
+	unset($result);
 
 	try {
 		$result = $client->executeCommand(new SoapParam($command, 'command'));
 	} catch(Exception $e) {
 		echo 'Item send exception: ', $e->getMessage(), "<br>\n";
+		$result = true; // Set the result so the loop can finish, since some items custom to other servers could fail.
 	}
 
 	if(!isset($result)) {
 		echo "Character item send failed!<br>\n";
 		return 1;
 	}
+}
+
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=mZero_characters;charset=utf8', $dbUsername, $dbPassword);
+$statement = $pdo->query("select guid from characters where name='" . $character_name . "'");
+$row = $statement->fetch(PDO::FETCH_ASSOC);
+$guid = $row['guid'];
+
+$statement = $pdo->query("select skill,max from character_skills where guid='" . $row['guid'] . "'");
+$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($rows as $row) {
+	$skill = $row['skill'];
+	$max = $row['max'];
+	$statement = $pdo->query("update character_skills set value='300',max='300' where skill='" . $skill . "'");
 }
 
 echo "Success!\n";
